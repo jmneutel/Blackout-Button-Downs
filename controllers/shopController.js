931de77c7088ceecf124b65a2870701bb3443271
Jared Neutel <Jared@Macintosh-2.local>
2017-05-15 19:41:30 -0700

    Procfile

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -29,33 +29,33 @@ router.get('/contact', function(req, res) {
 router.post('/contact/send', function(req, res, next) {
     var transporter = nodemailer.createTransport({
 
-    	service: 'Gmail',
-    	auth: {
-    		user: 'codeemailtester@gmail.com',
-    		pass: 'password12345'
-    	}
+        service: 'Gmail',
+        auth: {
+            user: 'codeemailtester@gmail.com',
+            pass: 'password12345'
+        }
     });
 
     var mailOptions = {
 
-    	from: 'Website User <codeemailtester@gmail.com>',
-    	to: 'jaredneutel@gmail.com',
-    	subject: 'Website Submission',
-    	text: 'You have a new submission with the folowing details...Name: '+req.body.first_name+' '+req.body.last_name+' Email: '+req.body.email+' Phone Number: '+req.body.phone+' Address: '+req.body.address+', '+req.body.city+', '+req.body.state+', '+req.body.zip+' Interested in being a rep: '+req.body.rep+' Message: '+req.body.comment,
-    	html: '<p>You got a new submission with the following details</p><ul><li>Name: '+req.body.first_name+' '+req.body.last_name+'</li><li>Email: '+req.body.email+'</li><li>Phone Number: '+req.body.phone+'</li><li>Address: '+req.body.address+', '+req.body.city+', '+req.body.state+', '+req.body.zip+'</li><li>Interested in being a rep: '+req.body.rep+'</li><li>Message: '+req.body.comment+'</li></ul>'
+        from: 'Website User <codeemailtester@gmail.com>',
+        to: 'jaredneutel@gmail.com',
+        subject: 'Website Submission',
+        text: 'You have a new submission with the folowing details...Name: ' + req.body.first_name + ' ' + req.body.last_name + ' Email: ' + req.body.email + ' Phone Number: ' + req.body.phone + ' Address: ' + req.body.address + ', ' + req.body.city + ', ' + req.body.state + ', ' + req.body.zip + ' Interested in being a rep: ' + req.body.rep + ' Message: ' + req.body.comment,
+        html: '<p>You got a new submission with the following details</p><ul><li>Name: ' + req.body.first_name + ' ' + req.body.last_name + '</li><li>Email: ' + req.body.email + '</li><li>Phone Number: ' + req.body.phone + '</li><li>Address: ' + req.body.address + ', ' + req.body.city + ', ' + req.body.state + ', ' + req.body.zip + '</li><li>Interested in being a rep: ' + req.body.rep + '</li><li>Message: ' + req.body.comment + '</li></ul>'
 
     };
 
-    transporter.sendMail(mailOptions, function(error, info){
+    transporter.sendMail(mailOptions, function(error, info) {
 
-    	if (error) {
-    		console.log(error);
-    		res.redirect('/');
-    	} else {
-    		console.log('Message Sent: '+info.response);
-    		res.redirect('/');
+        if (error) {
+            console.log(error);
+            res.redirect('/');
+        } else {
+            console.log('Message Sent: ' + info.response);
+            res.redirect('/');
 
-    	}
+        }
     })
 });
 
